309c8beeb414537f704e625075b5538775a4ca99
IT21217586 <it21217586@my.sliit.lk>
2024-05-10 22:46:07 +0530

    change footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,22 +11,25 @@ const Footer = () => {
             style={{ width: "150px", height: "auto" }}
           />
           <p className="my-5">
-            Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
-            purus et arcu massa dictum condimentum. Augue scelerisque iaculis
-            orci ut habitant laoreet. Iaculis tristique.
+            "Welcome to our online learning platform. We're dedicated to
+            providing top-notch education tailored to your needs. With expert
+            instructors and diverse courses, we're here to help you thrive. Join
+            us and start your journey toward success today."
           </p>
           <p>© Learnify All Rights Reserved.</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
-          <p className="my-5">Massa orci senectus</p>
-          <p className="my-5">Et gravida id et etiam</p>
-          <p>Ullamcorper vivamus</p>
+          <p className="my-5">Abot US</p>
+          <p className="my-5">learnifyStudentService123@gmail.com</p>
+          <p>learnifyFacebookstudent2.com</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p className="my-5">23/A Scentury Road,</p>
+          <p className="my-5">Marvin Mawatha,Negambo,</p>
+          <p className="my-5">Sri Lanka.</p>
+          <p>(112)425-6825</p>
         </div>
       </div>
     </footer>
