a444e8df24c9a3956bbcef5271f9f0e8a6e4e138
Krishna <krishnawanusha@gmail.com>
2024-05-10 23:08:41 +0530

    course page changes

--- src/pages/course/add.tsx
+++ src/pages/course/add.tsx
@@ -133,66 +133,6 @@ export default function CourseAddPage() {
           </div>
         </form>
       </section>
-
-      <section className="mx-auto mt-20 max-w-4xl rounded-md bg-white p-6 shadow-md dark:bg-gray-800">
-        <h2 className="text-lg font-semibold capitalize text-gray-700 dark:text-white">
-          Account settings
-        </h2>
-
-        <form>
-          <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2">
-            <div>
-              <label className="text-gray-700 dark:text-gray-200">
-                Username
-              </label>
-              <input
-                id="username"
-                type="text"
-                className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-500"
-              />
-            </div>
-
-            <div>
-              <label className="text-gray-700 dark:text-gray-200">
-                Email Address
-              </label>
-              <input
-                id="emailAddress"
-                type="email"
-                className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-500"
-              />
-            </div>
-
-            <div>
-              <label className="text-gray-700 dark:text-gray-200">
-                Password
-              </label>
-              <input
-                id="password"
-                type="password"
-                className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-500"
-              />
-            </div>
-
-            <div>
-              <label className="text-gray-700 dark:text-gray-200">
-                Password Confirmation
-              </label>
-              <input
-                id="passwordConfirmation"
-                type="password"
-                className="mt-2 block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 focus:border-blue-500 focus:outline-none focus:ring dark:border-gray-600 dark:bg-gray-800 dark:text-gray-300 dark:focus:border-blue-500"
-              />
-            </div>
-          </div>
-
-          <div className="mt-6 flex justify-end">
-            <button className="transform rounded-md bg-gray-700 px-6 py-2 leading-5 text-white transition-colors duration-200 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
-              Save
-            </button>
-          </div>
-        </form>
-      </section>
     </>
   );
 }
